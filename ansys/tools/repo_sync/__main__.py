12fe0982318f326ee065e784e05f38b3622605b4
Maxime Rey <maxime.rey@ansys.com>
2022-01-17 18:12:43 +0100

    Add default value of the argument 'organization'.

--- ansys/tools/repo_sync/__main__.py
+++ ansys/tools/repo_sync/__main__.py
@@ -12,7 +12,7 @@ from .repo_sync import synchronize_repositories
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Copy the content of a repository into an other repository.')
     parser.add_argument('-o', '--organization',
-                        help='Name of the organization.')
+                        help='Name of the organization. Default value is "pyansys".')
     parser.add_argument('-r', '--repository',
                         help='Name of the repository.')
     parser.add_argument('-t', '--token',
