6aa35fdc5e4717239511dc0680c25b287ced6a9f
Max <maxime.rey@ansys.com>
2022-01-18 17:37:45 +0100

    Fix style checking.

--- ansys/tools/repo_sync/_version.py
+++ ansys/tools/repo_sync/_version.py
@@ -3,12 +3,12 @@
 On the ``main`` branch, use 'dev0' to denote a development version.
 For example:
 
-version_info = 0, 58, 'dev0'
+version_info = 0, 58, "dev0"
 
 """
 
 # major, minor, patch
-version_info = 0, 1, 'dev0'
+version_info = 0, 1, "dev0"
 
 # Nice string for the version
-__version__ = '.'.join(map(str, version_info))
+__version__ = ".".join(map(str, version_info))
