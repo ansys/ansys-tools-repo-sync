95ba3d2e5a63e7ad8b8fb6dfb1cfbf54d7483431
Max <maxime.rey@ansys.com>
2022-02-18 11:46:43 +0100

    PackageNotFoundError.

--- ansys/tools/repo_sync/__init__.py
+++ ansys/tools/repo_sync/__init__.py
@@ -1,7 +1,7 @@
 """Tool to synchronize the content of different repositories."""
 try:
     import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
+except PackageNotFoundError:
     import importlib_metadata
 
 __version__ = importlib_metadata.version(__name__.replace(".", "-"))
