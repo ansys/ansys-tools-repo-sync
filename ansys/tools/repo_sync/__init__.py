b05850177e9d5776f9e425a156a42d048ae36657
Max <maxime.rey@ansys.com>
2022-02-18 11:13:47 +0100

    importlib.metadata

--- ansys/tools/repo_sync/__init__.py
+++ ansys/tools/repo_sync/__init__.py
@@ -1,3 +1,4 @@
 """Tool to synchronize the content of different repositories."""
-__version__ = importlib_metadata.version(__name__.replace(".", "-"))
+import importlib.metadata
+__version__ = importlib.metadata.version(__name__.replace(".", "-"))
 from .repo_sync import synchronize
