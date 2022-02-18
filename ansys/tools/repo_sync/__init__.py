7bd3d5b49774285ae537731727f536c9df405d4a
Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
2022-02-18 15:31:11 +0100

    Change the exception type to ModuleNotFoundError. (#61)

--- ansys/tools/repo_sync/__init__.py
+++ ansys/tools/repo_sync/__init__.py
@@ -1,7 +1,7 @@
 """Tool to synchronize the content of different repositories."""
 try:
     import importlib.metadata as importlib_metadata
-except PackageNotFoundError:
+except ModuleNotFoundError:
     import importlib_metadata
 
 __version__ = importlib_metadata.version(__package__ or __name__)
