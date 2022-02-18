19a0b8305a38d81d9e2346096f3bec6065912262
Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
2022-02-18 12:02:01 +0100

    Version is extracted from metadata. (#58)
    * Version is extracted from metadata.
    
    * importlib.metadata
    
    * Use importlib_metadata.
    
    * PackageNotFoundError.
    
    * The name of the package was not correct.
    
    * Use package or name.

--- ansys/tools/repo_sync/__init__.py
+++ ansys/tools/repo_sync/__init__.py
@@ -1,2 +1,8 @@
 """Tool to synchronize the content of different repositories."""
+try:
+    import importlib.metadata as importlib_metadata
+except PackageNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__package__ or __name__)
 from .repo_sync import synchronize
