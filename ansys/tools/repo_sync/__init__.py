3f778f5df96bb374b5d511ca2b55a03a24a485de
Max <maxime.rey@ansys.com>
2022-02-18 11:39:06 +0100

    Use importlib_metadata.

--- ansys/tools/repo_sync/__init__.py
+++ ansys/tools/repo_sync/__init__.py
@@ -1,4 +1,8 @@
 """Tool to synchronize the content of different repositories."""
-import importlib.metadata
-__version__ = importlib.metadata.version(__name__.replace(".", "-"))
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__.replace(".", "-"))
 from .repo_sync import synchronize
