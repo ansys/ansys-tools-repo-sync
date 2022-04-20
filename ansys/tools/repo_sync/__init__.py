b19bb51e3e75b66bc6cd7b11e64915b19a1f9a05
Max <maxime.rey@ansys.com>
2022-04-20 16:03:58 +0200

    Change the dependencies.

--- ansys/tools/repo_sync/__init__.py
+++ ansys/tools/repo_sync/__init__.py
@@ -1,8 +1,4 @@
 """Tool to synchronize the content of different repositories."""
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata
 
 __version__ = "0.1.dev0"
 from .repo_sync import synchronize
