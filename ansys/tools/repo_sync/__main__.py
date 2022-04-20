8adf1cd424334c687583fb84a71bb9dd6f458f84
Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
2022-04-20 15:11:21 +0200

    Reorder click options. (#118)

--- ansys/tools/repo_sync/__main__.py
+++ ansys/tools/repo_sync/__main__.py
@@ -10,9 +10,15 @@ from .repo_sync import synchronize as _synchronize
 
 
 @click.command(short_help="Copy the content of a repository into an other repository.")
-@click.option("--organization", "-o", type=str, help="Name of the organization.", default="pyansys")
+@click.option(
+    "--manifest",
+    "-m",
+    type=click.Path(dir_okay=False, exists=True),
+    help="Manifest to mention prohibited extension files.",
+)
 @click.option("--repository", "-r", type=str, help="Name of the repository.", required=True)
 @click.option("--token", "-t", type=str, help="Personal access token.")
+@click.option("--organization", "-o", type=str, help="Name of the organization.", default="pyansys")
 @click.option(
     "--protos",
     "-p",
@@ -21,24 +27,18 @@ from .repo_sync import synchronize as _synchronize
     required=True,
 )
 @click.option(
-    "--manifest",
-    "-m",
-    type=click.Path(dir_okay=False, exists=True),
-    help="Manifest to mention prohibited extension files.",
-)
-@click.option(
     "--dry-run",
     "-d",
     is_flag=True,
     default=False,
     help="Simulate the behavior of the synchronization without performing it.",
 )
-def synchronize(manifest, repository, token, organization, protos, dry_run):
+def synchronize(manifest, token, repository, organization, protos, dry_run):
     """CLI command to execute the repository synchronization."""
     _synchronize(
         manifest=manifest,
-        repository=repository,
         token=token,
+        repository=repository,
         organization=organization,
         protos_path=protos,
         dry_run=dry_run,
