b11071495e5f32bd5cfdb2779cc1c311773af480
Maxime Rey <87315832+MaxJPRey@users.noreply.github.com>
2022-09-13 21:35:40 +0200

    Add an argument to specify the name of the branch to check out. (#178)
    * Add an argument to specify the name of the branch to check out.
    
    * Add branch_checked_out missing argument.
    
    * Add branch name to the documentation.
    
    * Reorganize arguments.
    
    * Add unit test for the branch_checked_out argument.

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -35,8 +35,8 @@ class CaptureStdOut:
         return self._stream.getvalue()
 
 
-def test_synchronization():
-    """Test standard synchronization."""
+def test_synchronization_for_main_branch():
+    """Test standard synchronization from the main branch."""
 
     # Create a temp directory that will be used as a fake public repo
     with tempfile.TemporaryDirectory() as temp_dir:
@@ -63,6 +63,62 @@ def test_synchronization():
                 token=TOKEN,
                 repository="ansys-tools-repo-sync",
                 organization="ansys",
+                protos_path=os.path.join("assets", "ansys", "api", "test", "v0"),
+                dry_run=False,
+            )
+
+    gh = github.Github(TOKEN)
+    repo = gh.get_repo("ansys/ansys-tools-repo-sync")
+
+    for pull_request in repo.get_pulls():
+        if pull_request.title == "Add folder content from assets/ansys/api/test/v0.":
+            time.sleep(50)
+            pull_request.edit(state="closed")
+            # Following assertion is required to be sure that the PR was created
+            # and deleted properly. Otherwise, there is no way to make sure that it
+            # worked properly as it is deleted automatically.
+            assert True
+            break
+
+    branch_name = "sync/sync_branch"
+    try:
+        ref = repo.get_git_ref(branch_name)
+        ref.delete()
+    except UnknownObjectException:
+        print("No such branch", branch_name)
+
+    assert "Synchronization Succeeded..." in str(capture.content)
+
+
+def test_synchronization_not_for_main():
+    """Test standard synchronization from a branch different than main."""
+
+    # Create a temp directory that will be used as a fake public repo
+    with tempfile.TemporaryDirectory() as temp_dir:
+        os.chdir(temp_dir)
+
+        # Add control version to the temp directory
+        process = subprocess.Popen(
+            ["git", "init"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        stdout, stderr = process.communicate()
+
+        # copy proto file.
+        shutil.copytree(
+            os.path.join(ASSETS_DIRECTORY, "ansys", "api", "test", "v0"),
+            os.path.join(os.getcwd(), "assets", "ansys", "api", "test", "v0"),
+        )
+
+        capture = CaptureStdOut()
+        with capture:
+            synchronize(
+                manifest=os.path.join(ASSETS_DIRECTORY, "manifest.txt"),
+                token=TOKEN,
+                repository="ansys-tools-repo-sync",
+                organization="ansys",
+                branch_checked_out="test/test",
                 protos_path=os.path.join("assets", "ansys", "api", "test", "v0"),
                 dry_run=False,
             )
