d2468e036905fe15064d90e2303bffb20b207666
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2023-09-25 13:20:05 +0200

    test: include test if PR already exists (#1772)

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -1,7 +1,6 @@
 import os
 import shutil
 import subprocess
-import tempfile
 
 import pytest
 
@@ -54,6 +53,59 @@ def test_synchronize():
     finally:
         if result:
             cleanup_remote_repo(owner, repository, result)
+
+
+def test_synchronize_to_existing_pr():
+    """Test synchronization tool (when PR already exists)."""
+
+    # Define your test data here
+    owner = "ansys"
+    repository = "ansys-tools-repo-sync"
+    from_dir = os.path.join(ASSETS_DIRECTORY, "ansys")
+    to_dir = "src/ansys"
+    manifest = os.path.join(ASSETS_DIRECTORY, "manifest.txt")
+
+    # Call the function
+    result = None
+    try:
+        result = synchronize(
+            owner=owner,
+            repository=repository,
+            token=TOKEN,
+            from_dir=from_dir,
+            to_dir=to_dir,
+            include_manifest=manifest,
+            skip_ci=True,
+        )
+
+        # Assertions or validations
+        assert f"https://github.com/ansys/ansys-tools-repo-sync/pull/" in result
+
+        # Call the function again - and check that the PR already exists.
+        result_pr_already_exists = synchronize(
+            owner=owner,
+            repository=repository,
+            token=TOKEN,
+            from_dir=from_dir,
+            to_dir=to_dir,
+            include_manifest=manifest,
+            skip_ci=True,
+        )
+
+        # Verify the PR is the same
+        assert result_pr_already_exists == result
+
+        # Check that the proper modified files have been added
+        list_of_files = ["src/ansys/api/test/v0/hello_world.py", "src/ansys/api/test/v0/test.proto"]
+        assert check_files_in_pr(owner, repository, result, list_of_files)
+
+    except Exception as err:
+        raise err
+    finally:
+        if result:
+            cleanup_remote_repo(owner, repository, result)
+        if result_pr_already_exists and result != result_pr_already_exists:
+            cleanup_remote_repo(owner, result_pr_already_exists, result)
 
 
 def test_synchronize_with_only_proto_manifest():
@@ -174,14 +226,13 @@ def test_synchronize_with_cleanup_and_dry_run(capsys):
 
 
 @pytest.mark.skipif(SKIP_LOCALLY, reason="Only runs on workflow")
-def test_synchronize_from_cli():
+def test_synchronize_from_cli(tmpdir):
     """Test synchronization tool (without manifest) from CLI."""
 
     # Define a temp directory and copy assets in it
-    temp_dir = tempfile.TemporaryDirectory(prefix="repo_clone_cli_")
     shutil.copytree(
         ASSETS_DIRECTORY,
-        temp_dir.name,
+        tmpdir,
         dirs_exist_ok=True,
     )
 
@@ -218,7 +269,7 @@ def test_synchronize_from_cli():
         ],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
-        cwd=temp_dir.name,
+        cwd=tmpdir,
     )
 
     # Check output info
@@ -238,14 +289,13 @@ def test_synchronize_from_cli():
 
 
 @pytest.mark.skipif(SKIP_LOCALLY, reason="Only runs on workflow")
-def test_synchronize_with_only_proto_manifest_from_cli():
+def test_synchronize_with_only_proto_manifest_from_cli(tmpdir):
     """Test synchronization tool (with manifest) from CLI."""
 
     # Define a temp directory and copy assets in it
-    temp_dir = tempfile.TemporaryDirectory(prefix="repo_clone_cli_")
     shutil.copytree(
         ASSETS_DIRECTORY,
-        temp_dir.name,
+        tmpdir,
         dirs_exist_ok=True,
     )
 
@@ -282,7 +332,7 @@ def test_synchronize_with_only_proto_manifest_from_cli():
         ],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
-        cwd=temp_dir.name,
+        cwd=tmpdir,
     )
 
     # Check output info
@@ -302,7 +352,7 @@ def test_synchronize_with_only_proto_manifest_from_cli():
 
 
 @pytest.mark.skipif(SKIP_LOCALLY, reason="Only runs on workflow")
-def test_synchronize_with_cleanup_cli():
+def test_synchronize_with_cleanup_cli(tmpdir):
     """
     Test synchronization tool (with --clean-to-dir flag) from CLI.
 
@@ -313,10 +363,9 @@ def test_synchronize_with_cleanup_cli():
     """
 
     # Define a temp directory and copy assets in it
-    temp_dir = tempfile.TemporaryDirectory(prefix="repo_clone_cli_")
     shutil.copytree(
         ASSETS_DIRECTORY,
-        temp_dir.name,
+        tmpdir,
         dirs_exist_ok=True,
     )
 
@@ -355,7 +404,7 @@ def test_synchronize_with_cleanup_cli():
         ],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
-        cwd=temp_dir.name,
+        cwd=tmpdir,
     )
 
     # Check output info
@@ -375,14 +424,13 @@ def test_synchronize_with_cleanup_cli():
 
 
 @pytest.mark.skipif(SKIP_LOCALLY, reason="Only runs on workflow")
-def test_synchronize_with_no_sync_cli():
+def test_synchronize_with_no_sync_cli(tmpdir):
     """Test synchronization tool (with no files needed to be synced) from CLI."""
 
     # Define a temp directory and copy assets in it
-    temp_dir = tempfile.TemporaryDirectory(prefix="repo_clone_cli_")
     shutil.copytree(
         ASSETS_DIRECTORY,
-        temp_dir.name,
+        tmpdir,
         dirs_exist_ok=True,
     )
 
@@ -419,7 +467,7 @@ def test_synchronize_with_no_sync_cli():
         ],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
-        cwd=temp_dir.name,
+        cwd=tmpdir,
     )
 
     # Check output info
