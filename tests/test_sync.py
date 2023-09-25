795ebd7aa0dfd5ed41cdd25030fdf5d3c2443d25
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2023-09-25 12:28:23 +0200

    feat: use tmpdir fixture

--- tests/test_sync.py
+++ tests/test_sync.py
@@ -375,14 +375,13 @@ def test_synchronize_with_cleanup_cli():
 
 
 @pytest.mark.skipif(SKIP_LOCALLY, reason="Only runs on workflow")
-def test_synchronize_with_no_sync_cli():
+def test_synchronize_with_no_sync_cli(tmpdir):
     """Test synchronization tool (with no files needed to be synced) from CLI."""
 
     # Define a temp directory and copy assets in it
-    temp_dir = tempfile.TemporaryDirectory(prefix="repo_clone_cli_")
     shutil.copytree(
         ASSETS_DIRECTORY,
-        temp_dir.name,
+        tmpdir,
         dirs_exist_ok=True,
     )
 
@@ -419,7 +418,7 @@ def test_synchronize_with_no_sync_cli():
         ],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
-        cwd=temp_dir.name,
+        cwd=tmpdir,
     )
 
     # Check output info
