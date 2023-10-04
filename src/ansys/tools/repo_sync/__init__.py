0f3f87630ab16dc81dd609906d95ab153c50660d
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2023-10-04 12:06:36 +0200

    feat: bump to Python 3.12

--- src/ansys/tools/repo_sync/__init__.py
+++ src/ansys/tools/repo_sync/__init__.py
@@ -2,11 +2,7 @@
 
 # Version
 # ------------------------------------------------------------------------------
-
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:  # pragma: no cover
-    import importlib_metadata
+import importlib.metadata as importlib_metadata
 
 __version__ = importlib_metadata.version("ansys-tools-repo-sync")
 
